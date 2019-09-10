329f7efa12622fa715d1d2e882adc3f3f17aa365
Yury Shchegolev <cardiffc@yandex.ru>
2019-09-10 17:36:26 +0300

    Database in list Final

--- Main.java
+++ Main.java
@@ -6,44 +6,25 @@ import java.util.TreeSet;
 public class Main {
     public static void main(String[] args) {
         String[] letters = {"А АА", "В ВВ","Е ЕЕ","К КК","М ММ","Н НН","О ОО","Р РР","С СС","Т ТТ","У УУ","Х ХХ"};
-        ArrayList<String> regions = new ArrayList<>();
-        ArrayList<Integer> numbers = new ArrayList<>();
-        TreeSet<String> coolNumber = new TreeSet<>();
+        ArrayList<String> collNumbers = new ArrayList<>();
+        int count = 0;
 
-        for (Integer i = 1; i <= 198 ; i++) {
-            if (i < 10)
-            {
-                regions.add("0" + i.toString());
-            }
-            else {
-                regions.add(i.toString());
-            }
-        }
-
-        for (int i = 1; i <= 999 ; i++) {
-            if (i % 111 == 0) {
-                numbers.add(i);
-            }
-        }
-        regions.add("799");
-        regions.add("759");
-        regions.add("777");
-
-        for (int k = 0; k < regions.size() ; k++) {
-            for (int i = 0; i < numbers.size(); i++) {
-                for (int j = 0; j < letters.length; j++) {
-                    coolNumber.add((letters[j].replaceAll(" ", numbers.get(i).toString()).concat(regions.get(k))));
-
+        for (int i = 0; i < letters.length ; i++) {
+            for (Integer region = 0; region < 199 ; region++) {
+                for (Integer number = 1; number < 1000 ; number++) {
+                   if (number % 111 == 0)
+                   {
+                       if (region < 10)
+                       {
+                           collNumbers.add((letters[i].replaceAll(" ", number.toString())).concat("0".concat(region.toString())));
+                       } else {
+                           collNumbers.add((letters[i].replaceAll(" ", number.toString())).concat(region.toString()));
+                       }
+                   }
                 }
-
             }
+
         }
 
-
-        for (var number : coolNumber)
-        {
-            System.out.println(number);
-        }
-        System.out.println(coolNumber.size());
     }
 }
