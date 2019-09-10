f31347e33fbd2944d89dc0317e986113b81b9787
Yury Shchegolev <cardiffc@yandex.ru>
2019-09-10 14:18:13 +0300

    We have arraylist

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        String[] letters = {"A AA", "B BB"};
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = 1; i <= 999 ; i++) {
+            if (i % 111 == 0) {
+                numbers.add(i);
+
+            }
+        }
+
+        for (int i = 0; i < numbers.size() ; i++) {
+            System.out.println(numbers.get(i));
+
+        }
+
+
 
     }
 }
