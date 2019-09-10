bddfb2e69ef38684d5cfb2a34f8fc618230562ca
Yury Shchegolev <cardiffc@yandex.ru>
2019-09-10 14:40:41 +0300

    We have numbers

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        String[] letters = {"A AA", "B BB"};
+        String[] letters = {"A AA", "B BB","E EE","K KK","M MM","H HH","O OO","P PP","C CC","T TT","У УУ","X XX"};
+        ArrayList<Integer> regions = new ArrayList<>();
         ArrayList<Integer> numbers = new ArrayList<>();
+        int count = 0;
+
+        for (int i = 1; i <= 198 ; i++) {
+            regions.add(i);
+
+        }
 
         for (int i = 1; i <= 999 ; i++) {
             if (i % 111 == 0) {
@@ -12,12 +21,18 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < numbers.size() ; i++) {
-            System.out.println(numbers.get(i));
+        for (int k = 0; k < regions.size() ; k++) {
+            for (int i = 0; i < numbers.size(); i++) {
+                for (int j = 0; j < letters.length; j++) {
+                    System.out.println(letters[j].replaceAll(" ", numbers.get(i).toString()) + regions.get(k).toString());
+                    count++;
+                }
 
+            }
         }
+        HashSet<String> coolNumber = new HashSet();
 
 
-
+         System.out.println(count);
     }
 }
